81b1a594f4cff9a0137f4f1786329fac5189dc5f
Ian Watts <ian_f_watts@hotmail.com>
2023-03-01 16:38:33 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+var menuButton = document.getElementById("menu-button");
+var menu = document.getElementById("menu");
+
+menuButton.addEventListener("click", function() {
+    if (menu.style.display === "none") {
+        menu.style.display = "block";
+    } else {
+        menu.style.display = "none";
+    }
+});
